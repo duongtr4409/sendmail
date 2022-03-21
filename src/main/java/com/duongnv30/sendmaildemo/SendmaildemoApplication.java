ef367eb8f109fdf78f1625cd49b9d7e80be242ce
duongtora <nvduong216@gmail.com>
2022-03-21 22:15:41 +0700

    first commit: generate project with https://start.spring.io/

---
+++ src/main/java/com/duongnv30/sendmaildemo/SendmaildemoApplication.java
@@ -0,0 +1,13 @@
+package com.duongnv30.sendmaildemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendmaildemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendmaildemoApplication.class, args);
+	}
+
+}
